95b195cb2a6374aab94cc4efccfe77bf28d4c797
Octavio <octaviorassi@gmail.com>
2025-01-02 19:54:06 -0300

    progress on LRUQueue implementation

--- src/lru/lru.c
+++ src/lru/lru.c
@@ -17,21 +17,111 @@ struct _LRUQueue {
 
   pthread_mutex_t lock;
 
-  LRUNode start;
-  LRUNode end;
+  LRUNode first;  //  Recently used
+  LRUNode last;   //  Least used
 
 };
 
-inline LRUQueue lru_init() { return NULL; };
+LRUNode lru_node_create(LRUNode prev, LRUNode next, HashNode hashNode) {
 
-LRUNode lru_insert(LRUQueue q, int key, int value) {
+  LRUNode lru_node = malloc(sizeof(struct _LRUNode));
 
-  return NULL;
+  if (lru_node == NULL) {
+    // ! Mejorar el manejo de errores?
+    return NULL;
+  }
+
+  lru_node->next = next;
+  lru_node->prev = prev;
+  lru_node->hashNode = hashNode;
+
+  return lru_node;
+  
 }
 
-void lru_free_space(LRUQueue q, int key);
+void lru_node_destroy(LRUNode node) {
 
-void lru_delete(LRUQueue q, int k);
+  if (node == NULL) {
+    return;
+  }
 
-void lru_destroy(LRUQueue q);
+  hash_node_destroy(node->hashNode);
+  free(node);
+}
+
+
+LRUQueue lru_queue_init() { 
+
+  LRUQueue lru_queue = malloc(sizeof(struct _LRUQueue));
+
+  if (lru_queue == NULL) {
+    return NULL;
+  }
+
+  if (pthread_mutex_init(&lru_queue->lock, NULL) != 0) {
+    /*  Si no puede inicializarse el mutex todo el init falla,
+        liberamos la memoria y devolvemos null */
+    free(lru_queue); 
+    return NULL;
+  }
+
+};
+
+void lru_queue_insert(LRUQueue q, HashNode hashNode) {
+
+  if (q == NULL) {
+    return;
+  }
+
+  pthread_mutex_lock(&q->lock);
+
+  LRUNode second = q->first;
+
+  LRUNode new_lru_node = lru_node_create(second, NULL, hashNode);
+
+  // Si el nodo que antes era primero es no nulo, entonces su siguiente es el nuevo nodo
+  if (second != NULL) {
+    second->next = new_lru_node;
+  }
+
+  q->first = new_lru_node;
+
+  // Si la cola estaba vacia, entonces new_lru_node es tanto first como last
+  if (q->last == NULL) {
+    q->last = new_lru_node;
+  }
+
+  pthread_mutex_unlock(&q->lock);
+
+}
+
+// ! Cuantos eliminar? Si me pasan 0 como argumento elimino el ultimo, si es mayor a 0 busco el que me pidieron?
+void lru_queue_free_space(LRUQueue q, int key);
+
+void lru_queue_delete(LRUQueue q, int k);
+
+
+// ! Es mejor si la LRUQueue destruye la BucketList o la BucketList destruye a la LRU?
+// ! Obs. que asi como en lru_node_destroy llamo a hash_node_destroy, podria llamar a lru_node_destroy desde hash_node_destroy
+void lru_queue_destroy(LRUQueue q) {
+
+  if (q == NULL) {
+    return;
+  }
+
+  // ! Seria buena idea tomar el lock mientras elimino los nodos, y por ultimo devolverlo y liberarlo?
+  LRUNode tmp = q->first;
+  LRUNode next;
+
+  while (tmp) {
+    next = tmp->next;
+    lru_node_destroy(tmp);
+    tmp = next;
+  }
+
+  pthread_mutex_destroy(&q->lock);
+
+  free(q);
+
+};
 
--- src/lru/lru.h
+++ src/lru/lru.h
@@ -3,22 +3,33 @@
 
 typedef struct _LRUNode* LRUNode;
 
-typedef LRUNode LRUQueue;
+typedef struct _LRUQueue* LRUQueue;
+
+/*  @brief: Crea un nuevo LRUNodo con los valores prev, next, y hashNode.
+    @param[in]: prev es el nodo inmediatamente anterior en prioridad al nuevo nodo a crear
+    @param[in]: next es el nodo inmediatamente siguiente en prioridad al nuevo nodo a crear
+    @param[in]: es la informacion del hashNode asociada al nuevo LRUNode a crear */
+LRUNode lru_node_create(LRUNode prev, LRUNode next, HashNode hashNode);
+
+// @brief: Libera un LRUNode de manera no-recursiva.
+void lru_node_destroy(LRUNode node);
+
 
 // @brief: Inicializa una LRU vacia.
-LRUQueue lru_init();
+LRUQueue lru_queue_init();
 
-// @brief: Inicializa una LRU vacia.
-LRUNode lru_insert(LRUQueue q, int key, int value);
+// @brief: Inserta un nuevo nodo asociado al HashNode hashNode a la cola LRU
+// @param[in] hashNode el nodo de la tabla hash asociado
+void lru_queue_insert(LRUQueue q, HashNode hashNode);
 
 // @brief: Eliminar al elemento con menos prioridad de la cola.
-void lru_free_space(LRUQueue q, int key);
+void lru_queue_free_space(LRUQueue q, int key);
 
 // @brief Elimina el elemento asociado a la clave de la cola.
-// @params[in] k La clave del elemento a eliminar
-void lru_delete(LRUQueue q, int k);
+// @param[in] k La clave del elemento a eliminar
+void lru_queue_delete(LRUQueue q, int k);
 
 // @brief: Destruye la LRU liberando su memoria.
-void lru_destroy(LRUQueue q);
+void lru_queue_destroy(LRUQueue q);
 
 #endif // __LRU_H__
