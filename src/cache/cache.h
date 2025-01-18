72a2d25b9f69f8237797ae32ed69b4571fde6def
Octavio <octaviorassi@gmail.com>
2025-01-18 17:44:26 -0300

    includes comments

--- src/cache/cache.h
+++ src/cache/cache.h
@@ -1,24 +1,31 @@
 #ifndef __CACHE_H__
 #define __CACHE_H__
 
+#include "../helpers/results.h"
 
 typedef struct Cache* Cache;
 
-
-// @brief:
-// @param[out]:
-// @param[in]:
-// @return:
+/// todo: agregar que se pueda pasar los parametros por argumento.
+/// @brief Inicializa una cache con parametros por defecto.
 Cache cache_create();
 
-int cache_get(Cache cache, int key);
+/// @brief Devuelve el LookupResult resultante de buscar la clave asociada a la key en la cache.
+/// @return Un LookupResult con la clave asociada en caso de exito, o un status de error en caso contrario. 
+LookupResult cache_get(Cache cache, int key);
 
+/// @brief Inserta el par clave-valor en la cache dada. De no contarse con espacio suficiente, 
+/// aplica la politica de desalojo para generarlo.
 void cache_put(Cache cache, int key, int value);
 
-void cache_delete(Cache cache, int key);
 
+/// @brief Elimina el par clave-valor asociado a \a key en la cache objetivo. 
+/// @return -1 si se produjo un error, 0 si es exitoso, 1 si el par no estaba en la cache.
+int cache_delete(Cache cache, int key);
+
+/// @brief //todo imprime en pantalla las stats? las envia como string?
 void cache_stats(Cache cache);
 
+/// @brief Libera la memoria de todos los componentes de la cache.
 void cache_destroy(Cache cache);
 
 #endif // __CACHE_H__
