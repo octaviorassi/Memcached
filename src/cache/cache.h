7f09e5455b746ae9efddfabec04dcb680a75c730
Octavio <octaviorassi@gmail.com>
2025-01-21 17:02:19 -0300

    betters comments and style consistency

--- src/cache/cache.h
+++ src/cache/cache.h
@@ -5,27 +5,61 @@
 
 typedef struct Cache* Cache;
 
-/// todo: agregar que se pueda pasar los parametros por argumento.
-/// @brief Inicializa una cache con parametros por defecto.
-Cache cache_create();
+/**
+ *  @brief Inicializa una Cache con funcion de hash \a hash y cantidad de
+ *  buckets \a n_buckets.
+ * 
+ *  @param hash Funcion de hash a utilizar.
+ *  @param n_buckets Cantidad de buckets a crear.
+ * 
+ *  @return Un puntero a la cache creada.
+ */
+Cache cache_create(HashFunction hash, int n_buckets);
 
-/// @brief Devuelve el LookupResult resultante de buscar la clave asociada a la key en la cache.
-/// @return Un LookupResult con la clave asociada en caso de exito, o un status de error en caso contrario. 
-LookupResult cache_get(Cache cache, int key);
+/**
+ *  @brief Busca el valor asociado a una clave en la cache.
+ * 
+ *  @param key La clave buscada.
+ *  @param cache La cache objetivo
+ * 
+ *  @return Un LookUp result con un status indicando si la operacion fue exitosa.
+ */
+LookupResult cache_get(int key, Cache cache);
 
-/// @brief Inserta el par clave-valor en la cache dada. De no contarse con espacio suficiente, 
-/// aplica la politica de desalojo para generarlo.
-int cache_put(Cache cache, int key, int value);
+/**
+ *  @brief Inserta un par clave-valor en la cache. Si la clave ya estaba 
+ *  asociada a un valor, lo actualiza. De ser necesario, aplica la politica
+ *  de desalojo preestablecida para liberar memoria.
+ * 
+ *  @param key La clave.
+ *  @param val El valor asociado.
+ *  @param cache La cache objetivo
+ *  @return 0 en caso de exito, -1 si se produjo un error. 
+ */
+int cache_put(int key, int val, Cache cache);
 
 
-/// @brief Elimina el par clave-valor asociado a \a key en la cache objetivo. 
-/// @return -1 si se produjo un error, 0 si es exitoso, 1 si el par no estaba en la cache.
-int cache_delete(Cache cache, int key);
+/**
+ *  @brief Elimina el par clave-valor asociado a \a key en la cache objetivo.
+ * 
+ *  @param key La clave del par.
+ *  @param cache La cache objetivo.
+ * 
+ *  @return 0 si se elimina un par, 1 si no se encuentra el par, -1 si se
+ *  produjo un error.
+ */
+int cache_delete(int key, Cache cache);
 
-/// @brief //todo imprime en pantalla las stats? las envia como string?
+// todo
 void cache_stats(Cache cache);
 
-/// @brief Libera la memoria de todos los componentes de la cache.
+
+/**
+ *  @brief Libera la memoria de todas las componentes de la cache, incluida
+ *  esta ultima.
+ * 
+ *  @param cache La cache a destruir.
+ */
 void cache_destroy(Cache cache);
 
 #endif // __CACHE_H__
