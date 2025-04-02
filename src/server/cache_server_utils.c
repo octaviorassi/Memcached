799befc406b7f6d52e32186f3969b7e4b664e443
Octavio <octaviorassi@gmail.com>
2025-04-02 15:51:28 -0300

    parse_request now handles possibly incorrect command codes and implements minor fixes

--- src/server/cache_server_utils.c
+++ src/server/cache_server_utils.c
@@ -5,9 +5,6 @@
 
 // ? Problema: yo quiero que solo vea las funciones que expone la cache, no quiero que cache_utils pueda ver las funciones que expone cache_stats a cache. 
 
-int operations = 0;
-
-
 ssize_t recv_socket(int socket, char* message_buffer, int size, ClientData* cdata) {
 
   ssize_t total_bytes_received = 0;
@@ -57,7 +54,11 @@ ssize_t send_socket(int socket, char* message, int size) {
 
 }
 
-// ! cambiar la documentacion para reflear el nuevo return type
+
+static int command_is_valid(char cmd) {
+  return cmd == PUT || cmd == GET || cmd == DEL || cmd == STATS;
+}
+
 int parse_request(ClientData* cdata) {
 
   switch (cdata->parsing_stage) {
@@ -69,16 +70,21 @@ int parse_request(ClientData* cdata) {
       // No se termino de parsear el comando
       if (cdata->parsing_index < 1) return 0; 
 
-      // Cambio lo siguiente a hacer dependiendo del comando
+      // Si el comando no es valido, devolvemos -1 y se le cierra la conexion
+      if (!command_is_valid(cdata->command)) return -1;
+
+      // Si no, determinamos el estado de parseo dependiendo de si es STATS o no
       cdata->parsing_stage = cdata->command == STATS ?
-                                   PARSING_FINISHED : PARSING_KEY_LEN;
+                             PARSING_FINISHED : PARSING_KEY_LEN;
       cdata->parsing_index = 0;
+
       break;
 
     case PARSING_KEY_LEN:
 
-      if (recv_socket(cdata->socket, cdata->key_size_buffer +
-                      cdata->parsing_index, LENGTH - cdata->parsing_index, cdata) < 0) return -1;
+      if (recv_socket(cdata->socket,
+                      cdata->key_size_buffer + cdata->parsing_index,
+                      LENGTH - cdata->parsing_index, cdata) < 0) return -1;
 
       if (cdata->parsing_index < LENGTH) return 0;
 
@@ -87,17 +93,20 @@ int parse_request(ClientData* cdata) {
       cdata->parsing_stage = PARSING_KEY;
       cdata->parsing_index = 0;
       
-      return parse_request(cdata);
       break;
 
     case PARSING_KEY:
 
-      if (recv_socket(cdata->socket, cdata->key + cdata->parsing_index, cdata->key_size - cdata->parsing_index, cdata) < 0) return -1;
+      if (recv_socket(cdata->socket,
+                      cdata->key + cdata->parsing_index,
+                      cdata->key_size - cdata->parsing_index, cdata) < 0) return -1;
 
       if (cdata->parsing_index < cdata->key_size) return 0;
 
-      cdata->parsing_stage = cdata->command == PUT ? PARSING_VALUE_LEN : PARSING_FINISHED;
+      cdata->parsing_stage = cdata->command == PUT ?
+                             PARSING_VALUE_LEN : PARSING_FINISHED;
       cdata->parsing_index = 0;
+
       break;
 
     case PARSING_VALUE_LEN:
@@ -130,119 +139,92 @@ int parse_request(ClientData* cdata) {
       break;
   }
 
-  if (cdata->parsing_stage != PARSING_FINISHED) return parse_request(cdata); //? Ver si esto funciona bien
-}
+  if (cdata->parsing_stage != PARSING_FINISHED)
+    return parse_request(cdata);
 
+}
 
 extern Cache global_cache; //!! ELIMINAR
 
-
-static void serialize_stats_report(const StatsReport* report, char* buffer) {
-  
-  // Casteamos al buffer de chars a buffer de Counter para insertar mas facil.
-  Counter* buffer_ptr = (Counter*) buffer;
-
-  // Ahora insertamos en el buffer en el orden preestablecido, convirtiendo al network order.
-  buffer_ptr[0] = ntohl(report->put);
-  buffer_ptr[1] = ntohl(report->get);
-  buffer_ptr[2] = ntohl(report->del);
-  buffer_ptr[3] = ntohl(report->key);
-  buffer_ptr[4] = ntohl(report->evict);
-  buffer_ptr[5] = ntohl(report->allocated_memory);
-}
-
-
 int handle_request(ClientData* cdata) {
-
-  operations++;
 
   char command;
 
   switch (cdata->command) {
+
     case PUT:
 
-      int result = cache_put(cdata->key, cdata->key_size, cdata->value, cdata->value_size, global_cache);
+      int put_status = cache_put(cdata->key, cdata->key_size, cdata->value, cdata->value_size, global_cache);
       
-      if (result == 0) {
-        command = OKAY;
-        if (send_socket(cdata->socket, &command, 1) < 0) return -1;
-      } 
+      // Si el put fue exitoso, respondemos con OK, si no, con EUNK
+      command = put_status == 0 ? OKAY : EUNK;
 
-      else {
-        //TODO COMPLETAR
-      }
+      if (send_socket(cdata->socket, &command, 1) < 0) return -1;
 
       break;
   
-  case DEL:
+    case DEL:
       
-      result = cache_delete(cdata->key, cdata->key_size, global_cache);
+      int del_status = cache_delete(cdata->key, cdata->key_size, global_cache);
 
-      if (result == 0) command = OKAY;
-      else             command = ENOTFOUND;
+      command = del_status == 0 ? OKAY :
+               (del_status == 1 ? ENOTFOUND : EUNK);
 
       if (send_socket(cdata->socket, &command, 1) < 0) return -1;
       
       break;
   
-  case GET:
+    case GET:
   
-      LookupResult l_result = cache_get(cdata->key, cdata->key_size, global_cache);
+      LookupResult lr = cache_get(cdata->key, cdata->key_size, global_cache);
 
-      if (lookup_result_is_ok(l_result)) {
+      if (lookup_result_is_ok(lr)) {
         
         char command = OKAY;
         char length_buffer[LENGTH];
-        size_t size = ntohl(l_result.size);
+        size_t size = ntohl(lookup_result_get_size(lr));
         memcpy(length_buffer, &size, LENGTH);
 
-        // ! ESTO ROMPE LA INTERFAZ DE LOOKUPRESULT :(
         if (send_socket(cdata->socket, &command, 1) < 0) return -1;
         if (send_socket(cdata->socket, length_buffer, LENGTH) < 0) return -1;
-        if (send_socket(cdata->socket, l_result.ptr, l_result.size) < 0)
-          return -1;
-      }
-
-      else if (lookup_result_is_error(l_result)) {
-
-        printf("ERROR LOOKUP\n");
-        command = ENOTFOUND;
-        if (send_socket(cdata->socket, &command, 1) < 0) return -1;
+        if (send_socket(cdata->socket,
+                        lookup_result_get_value(lr),
+                        lookup_result_get_size(lr)) < 0) return -1;
       }
 
       else {
-        printf("MISS LOOKUP\n");
         command = ENOTFOUND;
         if (send_socket(cdata->socket, &command, 1) < 0) return -1;
       }
       
       break;
   
-  case STATS: 
+    case STATS: 
+      
+      // Obtengo el reporte de estadisticas.
+      StatsReport report = cache_report(global_cache);
+
+      // Creo un buffer donde cargar el mensaje y lo llenamos
+      size_t buffer_size = sizeof(char) * STATS_MESSAGE_LENGTH;
+      char report_buffer[buffer_size];
+      char command = STATS;
     
-    // Obtengo el reporte de estadisticas.
-    StatsReport report = cache_report(global_cache);
+      int report_len = stats_report_stringify(report, report_buffer);
 
-    // Creo un buffer donde cargar el mensaje y lo llenamos
-    size_t buffer_size = sizeof(char) * STATS_MESSAGE_LENGTH;
-    char report_buffer[buffer_size];
-    char command = STATS;
-  
-    int report_len = stats_report_stringify(report, report_buffer);
+      char length_buffer[LENGTH];
+      size_t size = ntohl(report_len);
+      memcpy(length_buffer, &size, LENGTH);
 
-    char length_buffer[LENGTH];
-    size_t size = ntohl(report_len);
-    memcpy(length_buffer, &size, LENGTH);
-
-    // Serializamos el reporte de estadisticas en el buffer y lo enviamos
-    send_socket(cdata->socket, &command, 1);                // Mando STATS 
-    send_socket(cdata->socket, length_buffer, LENGTH);      // Prefijo longitud
-    send_socket(cdata->socket, report_buffer, report_len);  // String del report
+      // Enviamos el comando el mensaje
+      send_socket(cdata->socket, &command, 1);                // Mando STATS 
+      send_socket(cdata->socket, length_buffer, LENGTH);      // Prefijo longitud
+      send_socket(cdata->socket, report_buffer, report_len);  // String del report
 
     break;
   
   }  
 }
+
 
 void reset_client_data(ClientData* cdata) {
 
@@ -262,6 +244,7 @@ int reconstruct_client_epoll(int epoll_fd, struct epoll_event* ev, ClientData* c
   return epoll_ctl(epoll_fd, EPOLL_CTL_MOD, cdata->socket, ev);
 
 } 
+
 
 int construct_new_client_epoll(int epoll_fd, ClientData* cdata) {
 
@@ -290,6 +273,7 @@ ClientData* create_new_client_data(int client_socket) {
   return new_cdata;
 }
 
+
 void delete_client_data(ClientData* cdata) {
 
   if (cdata == NULL)
@@ -301,6 +285,7 @@ void delete_client_data(ClientData* cdata) {
   free(cdata);
 
 }
+
 
 void drop_client(int epoll_fd, ClientData* cdata) {
   epoll_ctl(epoll_fd, EPOLL_CTL_DEL, cdata->socket, NULL);
